a751ff0ff724178ff19fe72e374d5450fa908740
SANCHIT AGARWAL <68566546+sanchit781@users.noreply.github.com>
2021-12-24 23:15:23 +0530

    Create DuplicateElement.java

---
+++ Assignment 2/Java Array/DuplicateElement.java
@@ -0,0 +1,12 @@
+public class DuplicateElement {  
+public static void main(String[] args) {  
+  
+        int [] arr = new int [] {1, 2, 3, 4, 5, 5, 5, 6, 7};  
+        System.out.println("Duplicate elements in given array: ");  
+        for(int i = 0; i < arr.length; i++) {  
+            for(int j = i + 1; j < arr.length; j++) {  
+                if(arr[i] == arr[j])  
+                    System.out.println(arr[j]);  
+            }  
+        }  
+    }  
